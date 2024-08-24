6c4f1af491fde4c696a21d0ebbe5da493e08d30f
nikitadarekar04 <darekarnikita10@gmail.com>
2024-08-24 20:38:43 +0530

    Update datatype.js

--- javascript/datatype.js
+++ javascript/datatype.js
@@ -1,14 +1,31 @@
 "use strict";//treat all js code as newer version
+//string
 let name="nikita"
-let age=18
-let isLoggedIn=false
-let state;
-
-
 console.log(typeof "nikita");
-console.log(typeof age);
-console.log(typeof false);
-
 console.log( "nikita");
+
+//number
+let age=18
+console.log(typeof age);
 console.log( age);
+
+//booleans
+let isLoggedIn=false
+console.log(typeof false);
 console.log( false);
+
+//undefined
+let state;
+console.log(typeof state);
+
+//null
+let a=null;
+console.log(typeof a);
+
+
+
+
+
+
+
+
