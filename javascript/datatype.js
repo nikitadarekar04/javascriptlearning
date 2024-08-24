658163da40ed761ef41fafde36b4a820beb26dd7
nikitadarekar04 <darekarnikita10@gmail.com>
2024-08-24 14:44:05 +0000

    variable

---
+++ javascript/datatype.js
@@ -0,0 +1,14 @@
+"use strict";//treat all js code as newer version
+let name="nikita"
+let age=18
+let isLoggedIn=false
+let state;
+
+
+console.log(typeof "nikita");
+console.log(typeof age);
+console.log(typeof false);
+
+console.log( "nikita");
+console.log( age);
+console.log( false);
---
+++ javascript/vari_con.js
@@ -0,0 +1,13 @@
+const accountId=1234
+let accountEmail="nikita@google.com"
+var accountPassword="45673"
+accountCity="latur"
+let accountState;
+
+
+accountEmail="nikki@.com"
+accountPassword="121212"
+accountCity="nanded"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState ]);
+
