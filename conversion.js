4c9112e9dd04b61335084d7f5d2067a9d0a6e1ea
nikitadarekar04 <darekarnikita10@gmail.com>
2024-08-24 15:51:56 +0000

    conversion

---
+++ conversion.js
@@ -0,0 +1,48 @@
+let a="33"
+console.log(typeof a);
+console.log(a);
+let b=Number(a)
+console.log(typeof b);
+console.log( b);
+
+let p="12abs"
+console.log(typeof p);
+console.log(p);
+let q=Number(p)
+console.log(typeof q);
+console.log(q);
+
+
+let x=""
+console.log(typeof x)
+console.log(x)
+let y = Boolean(x)
+console.log(typeof y)
+console.log(y)
+
+
+let e="xyz"
+console.log(typeof e)
+console.log(e)
+let f = Boolean(e)
+console.log(typeof f)
+console.log(f)
+
+
+
+let z=0
+console.log(typeof z)
+console.log(z)
+let w = Boolean(z)
+console.log(typeof w)
+console.log(w)
+
+let m=1
+console.log(typeof m)
+console.log(m)
+let n= Boolean(m)
+console.log(typeof n)
+console.log(n)
+
+
+
