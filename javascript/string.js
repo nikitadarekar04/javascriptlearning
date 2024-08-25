430540078352c1c48ed04f96913f4c97d1f836aa
nikitadarekar04 <darekarnikita10@gmail.com>
2024-08-25 11:37:22 +0000

    string method

---
+++ javascript/string.js
@@ -0,0 +1,57 @@
+//string Concat
+const name="Nikita"
+var age=20
+
+console.log(`My name is ${name} and My Age is ${age}`);
+
+//value pair Access
+const stringName= new String('Asmita')
+
+console.log(stringName[0]);
+
+//string length
+var a="durga"
+console.log(a.length);
+
+//charecter position
+var b="ankita"
+console.log(b.charAt(2));
+console.log(b.indexOf('k'));
+
+
+//string Uppercase method
+var c="pratiksha"
+console.log(c.toUpperCase());
+
+//SubString Method
+var d="dayanand"
+const e=a.substring(0,3)
+console.log(e);
+
+const f=a.slice(-8,3)
+console.log(f);
+
+//Trim Method
+var g="   yogita    "
+console.log(g);
+console.log(g.trim());
+
+//replace method
+const url="dayanand girls hostel,nanded"
+console.log(url.replace('nanded','Latur'));
+console.log(url.includes('hostel'));
+console.log(url.includes('Latur'));
+
+//split Method
+const p="this is the split method"
+console.log(p.split(' '));
+
+
+
+
+
+
+
+
+
+
