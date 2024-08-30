b255e7eb80276c595bd97332b588085e810af1ce
nikitadarekar04 <darekarnikita10@gmail.com>
2024-08-30 14:52:48 +0000

    number and math

---
+++ javascript/num_math.js
@@ -0,0 +1,48 @@
+//Number
+const score=200
+console.log(score);
+
+const a= Number(300)
+console.log(a);
+
+//toFix
+console.log(a.toFixed(3));
+
+//toPrecision
+const b=300.55
+console.log(b.toPrecision(3));
+
+//toLocalestring
+const c=12003400
+console.log(c.toLocaleString('en-IN'));
+
+//#####################################----MATH----############################################################
+
+      //abs()
+const num= -200.32
+console.log(Math);
+console.log(Math.abs(num));
+     //min
+
+console.log(Math.min(43,66,22,77));
+console.log(Math.max(43,55,67,77));
+
+//Ceil()
+console.log(Math.ceil(20.6));
+
+//round
+console.log(Math.round(20.7));
+
+//Floor()
+console.log(Math.floor(4.5));
+
+
+
+
+
+
+
+
+
+
+
