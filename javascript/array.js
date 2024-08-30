1ee94cf4b7c8905683ad6b81c55e27552e212553
nikitadarekar04 <darekarnikita10@gmail.com>
2024-08-30 17:35:28 +0000

    array in java script

---
+++ javascript/array.js
@@ -0,0 +1,43 @@
+//Array Declaration
+const arr=[1,2,3,4]
+console.log(arr[2]);
+
+const arr2=new Array(5,6,7,8);
+console.log(arr2[2]);
+
+//Array Method
+arr3=[2,6,8,9]
+arr3.push(7);
+arr4=[9,8,5,6]
+arr4.pop();
+arr5=[3,7,5,9]
+arr5.unshift(6);
+
+console.log(arr3);
+console.log(arr4);
+console.log(arr5);
+
+//array convert into string
+const arra=arr3.join()
+
+console.log(arr3);
+console.log(arra);
+
+//slice, splice
+
+const myarry=[6,9,7,3]
+console.log("A ",myarry);
+const arr6=myarry.slice(1,3)
+console.log(arr6);
+console.log("B ",myarry);
+const arr7=myarry.splice(1,3)
+console.log(arr67);
+
+
+
+
+
+
+
+
+
